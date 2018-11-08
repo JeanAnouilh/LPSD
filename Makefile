5e83955c031eb681979986a09f8d4089962a464e
Alexander Staub <alstaub@student.ethz.ch>
2018-11-08 15:34:46 +0100

    initial files

---
+++ Makefile
@@ -0,0 +1,37 @@
+CONTIKI_PROJECT = group-project
+
+ifdef COOJA
+TARGET ?= sky
+CFLAGS += -DCOOJA
+else
+TARGET ?= dpp-cc430
+endif
+
+CFLAGS += -DPLATFORM_$(shell echo $(TARGET) | tr a-z\- A-Z_)
+
+# select the data rate
+CFLAGS += -DDATARATE=1
+# select the sink address
+CFLAGS += -DSINK_ADDRESS=22
+# select the sink address
+CFLAGS += -DRANDOM_SEED=123
+
+PROJECT_SOURCEFILES += basic-radio.c data-generator.c
+
+all: $(CONTIKI_PROJECT)
+	$(info compiled for target platform $(TARGET) $(BOARD))
+	@msp430-size $(CONTIKI_PROJECT).$(TARGET)
+
+upload: $(CONTIKI_PROJECT).upload
+
+ifeq ($(TARGET),dpp-cc430)
+login: $(CONTIKI_PROJECT).login
+endif
+
+reset: $(CONTIKI_PROJECT).reset
+
+MAKE_MAC = MAKE_MAC_NULLMAC
+MAKE_NET = MAKE_NET_NULLNET
+CONTIKI = ../..
+include ../../tools/flocklab/Makefile.flocklab
+include $(CONTIKI)/Makefile.include
---
+++ group-project.c
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2018, Swiss Federal Institute of Technology (ETH Zurich).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Author:  Romain Jacob
+ */
+
+/*---------------------------------------------------------------------------*/
+/* general */
+#include "contiki.h"
+#include "node-id.h"
+/* GPIO */
+#include "gpio.h"
+/* radio */
+#include "basic-radio.h"
+/* data generator */
+#include "data-generator.h"
+/*---------------------------------------------------------------------------*/
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE    "DesignProjectApp"
+#define LOG_LEVEL LOG_LEVEL_MAIN
+#include <stdio.h> /* For printf() */
+/*---------------------------------------------------------------------------*/
+/* Makefile variables */
+#ifndef DATARATE
+#error No data rate specified. Example: use '-DDATARATE=10' to configure a rate of 10 packets per second.
+#endif /* DATARATE */
+uint16_t datarate = DATARATE;
+
+#ifndef SINK_ADDRESS
+#error No sink address specified. Example: use '-DSINK_ADDRESS=1' to configure the sink node ID.
+#endif /* SINK_ADDRESS */
+uint16_t sinkaddress = SINK_ADDRESS;
+
+#ifndef RANDOM_SEED
+#error No random seed specified. Example: use '-RANDOM_SEED=123' initialize the random number generator.
+#endif /* RANDOM_SEED */
+uint16_t randomseed = RANDOM_SEED;
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief     Struct to store packet information
+ */
+typedef struct {
+  uint16_t        src_id;
+  uint8_t         seqn;
+  uint16_t        payload;
+} lpsd_packet_t;
+/*---------------------------------------------------------------------------*/
+PROCESS(design_project_process, "Skeleton code - LPSD Design Project");
+AUTOSTART_PROCESSES(&design_project_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(design_project_process, ev, data)
+{
+  static lpsd_packet_t* packet;     /* packet pointer */
+  static lpsd_packet_t  packet_rcv; /* received packet buffer */
+  static uint8_t        packet_len; /* packet length, in Bytes */
+  static uint16_t       timeout_ms; /* packet receive timeout, in ms */
+
+  PROCESS_BEGIN();
+
+  /* initialize the data generator */
+  data_generation_init();
+
+  /* configure GPIO as outputs */
+  //PIN_CFG_OUT(RADIO_START_PIN);
+  PIN_CFG_OUT(RADIO_RX_PIN);
+  PIN_CFG_OUT(RADIO_TX_PIN);
+  PIN_CFG_OUT(LED_STATUS);
+
+  timeout_ms = 100;
+
+ 
+
+if(sinkaddress == 22)
+{
+  if(node_id == )
+  {
+
+  }
+  else if()
+  
+   while(1) {
+
+    /* listen for incoming packet */
+    packet_len = radio_rcv(((uint8_t*)&packet_rcv), timeout_ms);
+
+    /* if we received something, retransmit */
+    if(packet_len){
+      if(node_id == sinkaddress) {
+        /* --- SINK --- */
+        /* Write received message to serial */
+        LOG_INFO("Pkt:%u,%u,%u\n", packet_rcv.src_id,packet_rcv.seqn, packet_rcv.payload);
+
+      } else {
+        /* --- SOURCE --- */
+        /* Forward the packet */
+        radio_send(((uint8_t*)&packet_rcv),packet_len,1);
+        LOG_INFO("Packet received and forwarded...\n");
+      }
+    }
+
+    /* Check for packet in queue */
+    while(is_data_in_queue()){
+      packet = pop_data();
+      if(node_id == sinkaddress) {
+        /* --- SINK --- */
+        /* Write our own message to serial */
+        LOG_INFO("Pkt:%u,%u,%u\n", packet->src_id,packet->seqn, packet->payload);
+      } else {
+        /* --- SOURCE --- */
+        /* Send our packet */
+        radio_send(((uint8_t*)packet),sizeof(lpsd_packet_t),1);
+        LOG_INFO("Packet sent (seqn: %u)\n", packet->seqn);
+      }
+    }
+  }
+}
+else
+{
+   while(1) {
+
+    /* listen for incoming packet */
+    packet_len = radio_rcv(((uint8_t*)&packet_rcv), timeout_ms);
+
+    /* if we received something, retransmit */
+    if(packet_len){
+      if(node_id == sinkaddress) {
+        /* --- SINK --- */
+        /* Write received message to serial */
+        LOG_INFO("Pkt:%u,%u,%u\n", packet_rcv.src_id,packet_rcv.seqn, packet_rcv.payload);
+
+      } else {
+        /* --- SOURCE --- */
+        /* Forward the packet */
+        radio_send(((uint8_t*)&packet_rcv),packet_len,1);
+        LOG_INFO("Packet received and forwarded...\n");
+      }
+    }
+
+    /* Check for packet in queue */
+    while(is_data_in_queue()){
+      packet = pop_data();
+      if(node_id == sinkaddress) {
+        /* --- SINK --- */
+        /* Write our own message to serial */
+        LOG_INFO("Pkt:%u,%u,%u\n", packet->src_id,packet->seqn, packet->payload);
+      } else {
+        /* --- SOURCE --- */
+        /* Send our packet */
+        radio_send(((uint8_t*)packet),sizeof(lpsd_packet_t),1);
+        LOG_INFO("Packet sent (seqn: %u)\n", packet->seqn);
+      }
+    }
+  }
+}
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
